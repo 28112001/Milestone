ff9d723e001d71963a767cefbc4b0a19231a2d79
28112001 <aaronstains2001@gmail.com>
2022-10-31 17:51:39 +0530

    Miles

---
+++ Book/src/App.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import Front from "./Components/Header";
+
+const Head = () => {
+  return <Front />;
+};
+export default Head;
+/*import "./App.css";
+import React from "react";
+import Header from "./head";
+
+function App() {
+  const [dropdown, setDropdown] = React.useState(false);
+  const header = (
+    <div className="Topnav">
+      <h3>LOGO</h3>
+      <a href="#">Home</a>
+      <div className="Dropdown">
+        <div
+          className="dropbtn_content"
+          onMouseEnter={() => setDropdown(true)}
+          onMouseLeave={() => setDropdown(false)}
+        >
+          <button class="dropbtn">
+            Book
+            <i class="fa fa-caret-down"></i>
+          </button>
+          {dropdown && (
+            <div className="list">
+              <li>
+                <a href="#Book">Book Ur Appoints</a>
+              </li>
+              <li>
+                <a href="#Book">Update Ur Aadhaar</a>
+              </li>
+            </div>
+          )}
+        </div>
+      </div>
+      <a href="#">Our Services</a>
+      <a href="#">About Us</a>
+      <div className="but">
+        <button>LOGIN</button>
+      </div>
+    </div>
+  );
+
+  const head = (
+    <div className="hero">
+      <h2>
+        Government Of India
+        <br />
+        E-Service Website
+      </h2>
+    </div>
+  );
+
+  return (
+    <React.Fragment>
+      {head}
+      {header}
+
+      <Header />
+    </React.Fragment>
+  );
+}
+
+export default App;*/
---
+++ Book/src/head.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Head = () => {
+  return (
+    <div className="Topnav">
+      <h3>LOGO</h3>
+      <a href="#">Home</a>
+      <a href="#">Our Services</a>
+      <a href="#">About us</a>
+    </div>
+  );
+};
+
+export default Header;
